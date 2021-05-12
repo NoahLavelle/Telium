dad002fc228f6996a0a6c8a49c2494d7e0a1a4c2
BuildTools <noah.lavelle@gmail.com>
2021-05-12 07:04:17 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Telium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
